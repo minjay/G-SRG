281f41e01a0665d6f6b3d35c67368bbdb78132a3
minjay <mjfan@ucar.edu>
2017-02-14 21:30:46 +0800

    Add comments and change variable names.

--- cmp_nbr.m
+++ cmp_nbr.m
@@ -1,12 +1,14 @@
-function nbr_set = cmp_nbr( seed_set, adj_mat, n, labeled_set )
-
+function nbr_cells = cmp_nbr( seed_set, adj_mat, n, labeled_cells )
 % compute neighbors of a seed set
-diff_set = setdiff(1:n, [seed_set labeled_set]);
-nbr_set = [];
+
+% exclude seed_set itself and labeled_cells since they cannot be neighbors
+diff_set = setdiff(1:n, [seed_set labeled_cells]);
+
+nbr_cells = [];
 for i = diff_set
     for j = seed_set
         if adj_mat(i, j)==1
-            nbr_set = [nbr_set i];
+            nbr_cells = [nbr_cells i];
             break
         end
     end
