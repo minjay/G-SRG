0eb55862589114242a9a12c29426f340521364d9
minjay <mjfan@ucar.edu>
2017-02-16 09:59:13 +0800

    Add comments.

--- get_adj_mat.m
+++ get_adj_mat.m
@@ -1,6 +1,8 @@
 function adj_mat = get_adj_mat( E, n )
-
 % transform edges to adjacent matrix
+% E is obtained from function edges
+% adj_mat: adjacent matrix, 1 represents that there is an edge
+
 adj_mat = zeros(n);
 for i = 1:size(E, 1)
     adj_mat(E(i, 1), E(i, 2)) = 1;
