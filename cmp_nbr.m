418d72b99e047ed6878443d5951a527292c72f43
minjay <mjfan@ucar.edu>
2017-09-10 12:18:38 -0700

    Simplify the step of finding neighbors.

--- cmp_nbr.m
+++ cmp_nbr.m
@@ -4,14 +4,7 @@ function nbr_cells = cmp_nbr( seed_set, adj_mat, n, labeled_cells )
 % exclude seed_set itself and labeled_cells since they cannot be neighbors
 diff_set = setdiff(1:n, [seed_set labeled_cells]);
 
-nbr_cells = [];
-for i = diff_set
-    for j = seed_set
-        if adj_mat(i, j)
-            nbr_cells = [nbr_cells i];
-            break
-        end
-    end
-end
+% compute sum of each row
+nbr_cells = diff_set(sum(adj_mat(diff_set, seed_set), 2)>0);
 
 end
