e297bfa39f12ab4dc3522be50b78f2c3d2e87be8
minjay <mjfan@ucar.edu>
2018-10-29 22:27:46 -0700

    Add detailed comments of function cmp_nbr.

--- cmp_nbr.m
+++ cmp_nbr.m
@@ -1,10 +1,20 @@
 function nbr_cells = cmp_nbr( seed_set, adj_mat, n, labeled_cells )
-% compute neighbors of a seed set
+% Computes neighboring cells of a seed/growing region set.
+%
+% Args:
+%   seed_set: Seed/growing region set consisting of a number of cells.
+%   adj_mat: Adjacent matrix measuring distance between pairs of cells.
+%   n: Total number of cells.
+%   labeled_cells: Cells that have already been labeled.
+%
+% Returns:
+%   nbr_cells: Neighboring cells of the seed/growing region set.
 
-% exclude seed_set itself and labeled_cells since they cannot be neighbors
+% Excludes seed_set itself and labeled_cells since they cannot be
+% neighbors.
 diff_set = setdiff(1:n, [seed_set labeled_cells]);
 
-% compute sum of each row
+% Computes sum of each row.
 nbr_cells = diff_set(sum(adj_mat(diff_set, seed_set), 2)>0);
 
 end
